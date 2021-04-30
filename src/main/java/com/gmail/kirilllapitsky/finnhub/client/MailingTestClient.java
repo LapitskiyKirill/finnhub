153458bf330c61ad8c813afca1f89dd2ec6e3ad8
Kirill Lapitskii <kirill_10b@mail.ru>
2021-04-30 16:48:46 +0300

    MailingTestClient fix

--- src/main/java/com/gmail/kirilllapitsky/finnhub/client/MailingTestClient.java
+++ src/main/java/com/gmail/kirilllapitsky/finnhub/client/MailingTestClient.java
@@ -3,7 +3,7 @@ package com.gmail.kirilllapitsky.finnhub.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "MailingClient", url = "${mailing.path}")
+@FeignClient(name = "MailingTestClient", url = "${mailing.path}")
 public interface MailingTestClient {
     @PostMapping(value = "/verify")
     void verify();
