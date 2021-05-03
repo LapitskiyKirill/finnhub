695c3786f026326cf3ca0fd27913f5e692e4d894
Kirill Lapitskii <kirill_10b@mail.ru>
2021-05-03 16:55:48 +0300

    Repository fix

--- src/main/java/com/gmail/kirilllapitsky/finnhub/repository/PaymentRepository.java
+++ src/main/java/com/gmail/kirilllapitsky/finnhub/repository/PaymentRepository.java
@@ -2,15 +2,8 @@ package com.gmail.kirilllapitsky.finnhub.repository;
 
 import com.gmail.kirilllapitsky.finnhub.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    @Query(value = "SELECT payment FROM Payment payment where " +
-            "payment.paymentId = :id")
-    Optional<Payment> findById(@Param("id") String id);
+public interface PaymentRepository extends JpaRepository<Payment, String> {
 }
